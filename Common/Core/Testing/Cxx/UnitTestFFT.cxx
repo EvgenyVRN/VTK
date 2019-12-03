a31b8c6f2dc67038bae9bce72a57a717151fb82a
Evgeny Kolosov <ekolosov@inobitec.com>
2019-12-03 11:09:42 +0300

    Minor fixes in UnitTestFFT

--- Common/Core/Testing/Cxx/UnitTestFFT.cxx
+++ Common/Core/Testing/Cxx/UnitTestFFT.cxx
@@ -20,6 +20,13 @@
 #include <algorithm>
 #include <memory>
 
+
+static bool FuzzyCompare(const ComplexNumber& result, const ComplexNumber& test, double epsilon)
+{
+  return (vtkFFT::complex_module(result - test) < epsilon*epsilon);
+}
+
+
 static int Test_fft_direct(); // TODO: not implemented yet
 static int Test_fft_inverse();
 static int Test_complexes_to_doubles();
@@ -48,6 +55,77 @@ int UnitTestFFT(int, char* [])
 
 int Test_fft_direct()
 {
+// compare with numpy
+//  int status = 0;
+//  std::cout << "Test_fft_direct..";
+
+//  static constexpr auto inputSize = 50;
+//  static constexpr auto spectrumSize = 25;
+
+//  // test data
+//  auto i = 0;
+//  std::array<ComplexNumber, spectrumSize> test;
+//  // result of np.sin(np.arange(50))
+//  test[i++] = ComplexNumber(1.6325e-01, +0.0000e+00);
+//  test[i++] = ComplexNumber(1.6381e-01, +3.6392e-02);
+//  test[i++] = ComplexNumber(1.6560e-01, +7.6177e-02);
+//  test[i++] = ComplexNumber(1.6903e-01, +1.2400e-01);
+//  test[i++] = ComplexNumber(1.7506e-01, +1.8809e-01);
+//  test[i++] = ComplexNumber(1.8604e-01, +2.8696e-01);
+//  test[i++] = ComplexNumber(2.0931e-01, +4.7599e-01);
+//  test[i++] = ComplexNumber(2.8296e-01, +1.0408e+00);
+//  test[i++] = ComplexNumber(-3.1623e+00, -2.4749e+01);
+//  test[i++] = ComplexNumber(2.4794e-03, -1.0365e+00);
+//  test[i++] = ComplexNumber(6.7456e-02, -5.3945e-01);
+//  test[i++] = ComplexNumber(8.9422e-02, -3.6513e-01);
+//  test[i++] = ComplexNumber(1.0032e-01, -2.7419e-01);
+//  test[i++] = ComplexNumber(1.0675e-01, -2.1710e-01);
+//  test[i++] = ComplexNumber(1.1093e-01, -1.7709e-01);
+//  test[i++] = ComplexNumber(1.1383e-01, -1.4690e-01);
+//  test[i++] = ComplexNumber(1.1593e-01, -1.2287e-01);
+//  test[i++] = ComplexNumber(1.1749e-01, -1.0293e-01);
+//  test[i++] = ComplexNumber(1.1867e-01, -8.5828e-02);
+//  test[i++] = ComplexNumber(1.1957e-01, -7.0752e-02);
+//  test[i++] = ComplexNumber(1.2026e-01, -5.7147e-02);
+//  test[i++] = ComplexNumber(1.2078e-01, -4.4614e-02);
+//  test[i++] = ComplexNumber(1.2116e-01, -3.2851e-02);
+//  test[i++] = ComplexNumber(1.2142e-01, -2.1622e-02);
+//  test[i++] = ComplexNumber(1.2157e-01, -1.0730e-02);
+//  test[i++] = ComplexNumber(1.2162e-01, +0.0000e+00);
+
+//  // input data
+//  i = 0;
+//  std::array<double, inputSize> input;
+//  std::generate(input.begin(), input.end(), [&i]() { return std::sin(i++); });
+
+//  // fft
+//  int outSize;
+//  ComplexNumber* result = nullptr;
+//  vtkFFT::fft_direct(input.data(), inputSize, &outSize, result);
+
+//  // compare
+//  for(i=0; i<spectrumSize; i++)
+//  {
+
+//      if (!FuzzyCompare(result[i], test[i], 1e-03))
+//      {
+//        std::cout << "Expected " << test[i] << " but got " << result[i] << " difference is "
+//                  << result[i] - test[i] << std::endl;
+//        status++;
+//      }
+//  }
+
+//  delete[] result;
+
+//  if (status)
+//  {
+//    std::cout << "..FAILED" << std::endl;
+//  }
+//  else
+//  {
+//    std::cout << ".PASSED" << std::endl;
+//  }
+//  return status;
   return Test_fft_direct_inverse();
 }
 
